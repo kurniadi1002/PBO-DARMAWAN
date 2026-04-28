973b67a6883bf18e8445aa39fb8967b31d58dd3d
kurniadi1002 <darmawan102001@gmail.com>
2026-04-28 20:43:56 +0700

    Add files via upload

---
+++ pertemuan6/BreakAndcontinue.java
@@ -0,0 +1,10 @@
+public class BreakAndcontinue {
+
+    public static void main (String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            
+            System.out.println("perulangan ke-" +i);
+            if (i == 4) break;
+        }
+    }
+}

f86b876f35a3a7789f377f4e7591292f249bca96
kurniadi1002 <darmawan102001@gmail.com>
2026-06-02 19:45:09 +0700

    Create Lat2.java

---
+++ Pertemuan10/Lat2.java
@@ -0,0 +1,52 @@
+
+ import java.util.Scanner;
+
+// Kelas Enkapsulasi untuk Kalkulator
+class Kalkulator {
+    private double nilaiSatu;
+    private double nilaiDua;
+
+    // Setter
+    public void setNilaiSatu(double nilaiSatu) {
+        this.nilaiSatu = nilaiSatu;
+    }
+
+    public void setNilaiDua(double nilaiDua) {
+        this.nilaiDua = nilaiDua;
+    }
+
+    // Metode Aritmatika
+    public double tambah() { return nilaiSatu + nilaiDua; }
+    public double kurang() { return nilaiSatu - nilaiDua; }
+    public double kali() { return nilaiSatu * nilaiDua; }
+    public double bagi() { 
+        if (nilaiDua == 0) {
+            System.out.println("Error: Pembagian dengan nol tidak bisa dilakukan.");
+            return 0;
+        }
+        return nilaiSatu / nilaiDua; 
+    }
+}
+
+/// Main Class
+public class Lat2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Kalkulator kal = new Kalkulator();
+
+        System.out.println("=== Kalkulator Aritmatika ===");
+        System.out.print("Masukkan Nilai Satu: ");
+        kal.setNilaiSatu(input.nextDouble());
+
+        System.out.print("Masukkan Nilai Dua: ");
+        kal.setNilaiDua(input.nextDouble());
+
+        System.out.println("\n--- Hasil Operasi ---");
+        System.out.println("Penjumlahan : " + kal.tambah());
+        System.out.println("Pengurangan : " + kal.kurang());
+        System.out.println("Perkalian   : " + kal.kali());
+        System.out.println("Pembagian   : " + kal.bagi());
+
+        input.close();
+    }
+}
